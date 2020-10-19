4942ad1fe0c938ec29c1c085d271cceb03f74030
moltiplico <moltiplico@ukr.net>
2020-10-20 01:52:35 +0300

    fix sliders(vertical line & hide scroll)

--- js/sliders.js
+++ js/sliders.js
@@ -2,6 +2,7 @@ var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     // direction: 'vertical',
     loop: true,
+    spaceBetween: 10,
 
     // If we need pagination
     pagination: {
